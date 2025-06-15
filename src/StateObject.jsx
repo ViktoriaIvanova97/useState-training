66c5c2a3fd6ede0d6bae267f42b2a671fcf05b9f
Ivanova Viktoria <ivanovaviktoria97@yandex.by>
2025-06-15 17:01:45 +0300

    add state-object

---
+++ src/StateObject.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useState } from "react";
+
+const StateObject = () => {
+  const [user, setUser] = useState({
+    name: "Иван",
+    age: 25,
+    isActive: true,
+  });
+
+  const changeName = () => {
+    setUser((prevUser) => ({
+      ...prevUser,
+      name: prevUser.name === "Иван" ? "Петр" : "Иван",
+    }));
+  };
+
+  const increaseAge = () => {
+    setUser((prevUser) => ({
+      ...prevUser,
+      age: prevUser.age + 1,
+    }));
+  };
+
+  const changeActive = () => {
+    setUser((prevUser) => ({
+      ...prevUser,
+      isActive: prevUser.isActive ? false : true,
+    }));
+  };
+  return (
+    <>
+      <div>
+        <p>Имя:{user.name}</p>
+      </div>
+      <div>
+        <p>Возраст:{user.age}</p>
+      </div>
+      <div>
+        <p>Активен:{user.isActive ? "да" : "нет"}</p>
+      </div>
+      <button onClick={changeName}>Сменить имя</button>
+      <button onClick={increaseAge}>Увеличить возраст</button>
+      <button onClick={changeActive}>Переключить активность</button>
+    </>
+  );
+};
+
+export default StateObject;
